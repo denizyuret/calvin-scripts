2f5019521314d639e16631b93ecd29e10858d04c
Deniz Yuret <denizyuret@gmail.com>
2023-02-17 11:48:01 +0300

    .

--- calvin_extract_numbers.jl
+++ calvin_extract_numbers.jl
@@ -4,8 +4,17 @@ np = pyimport("numpy")
 for f in readdir()
     m = match(r"episode_(\d{7})\.npz", f)
     if m != nothing
-        id = m.captures[1]
+        idnum = m.captures[1]
         data = np.load(f, allow_pickle=true)
+        actions = get(data, "actions") # 1-7(7)
+        rel_actions = get(data, "rel_actions") # 8-14(7)
+        robot_obs = get(data, "robot_obs") # 15-29(15)
+        scene_obs = get(data, "scene_obs") # 30-53(24)
+        print(idnum, '\t')
+        writedlm(stdout, vcat(actions, rel_actions, robot_obs, scene_obs)')
+    end
+end
+
 # julia> for f in data[:files]; println(f, "\t", summary(get(data, f))); end
 # actions	7-element Vector{Float64}
 # rel_actions	7-element Vector{Float64}
@@ -17,10 +26,3 @@ for f in readdir()
 # depth_static	200×200 Matrix{Float32}
 # depth_gripper	84×84 Matrix{Float32}
 # depth_tactile	160×120×2 Array{Float32, 3}
-        actions = get(data, "actions") # 1-7(7)
-        rel_actions = get(data, "rel_actions") # 8-14(7)
-        robot_obs = get(data, "robot_obs") # 15-29(15)
-        scene_obs = get(data, "scene_obs") # 30-53(24)
-        writedlm(stdout, vcat(actions, rel_actions, robot_obs, scene_obs)')
-    end
-end
--- calvin_extract_numbers.py
+++ calvin_extract_numbers.py
@@ -3,13 +3,14 @@ import os
 import re
 import sys
 
-for f in os.listdir('.'):
+for f in sorted(os.listdir('.')):
     m = re.match(r"episode_(\d{7})\.npz", f)
     if m is not None:
-        id = m.group(1)
+        idnum = m.group(1)
         data = np.load(f, allow_pickle=True)
         actions = data.get("actions")  # 1-7(7)
         rel_actions = data.get("rel_actions")  # 8-14(7)
         robot_obs = data.get("robot_obs")  # 15-29(15)
         scene_obs = data.get("scene_obs")  # 30-53(24)
+        print(idnum, end='\t')
         print(*np.concatenate((actions, rel_actions, robot_obs, scene_obs)), sep='\t')
