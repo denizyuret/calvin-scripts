a0aad38f4dcea589af3c3036c997385a8e9ad36d
Deniz Yuret <denizyuret@gmail.com>
2023-02-20 17:32:01 +0300

    *** empty log message ***

---
+++ calvin_visualize.py
@@ -0,0 +1,111 @@
+# Usage: cd /datasets/calvin/D/validation; python calvin_visualize.py
+
+import sys
+import os
+import re
+import pdb
+import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk
+from tqdm import tqdm
+root = tk.Tk()
+
+def float2uint8(a):
+    return (255 * (a - a.min()) / (a.max() - a.min() + sys.float_info.epsilon)).astype(np.uint8)
+
+def array2image(a):
+    if np.issubdtype(a.dtype, np.floating):
+        a = float2uint8(a)
+    return ImageTk.PhotoImage(Image.fromarray(a))
+
+def numeric_fields(npz, idnum):
+    indexstr    = f"frame: {idnum}"
+    a = npz['actions']
+    actions     = f"act:  x:{a[0]: 5.2f} y:{a[1]: 5.2f} z:{a[2]: 5.2f} a:{a[3]: 5.2f} b:{a[4]: 5.2f} c:{a[5]: 5.2f} grp:{a[6]: 5.2f}"
+    b = npz['rel_actions']
+    rel_actions = f"rel:  x:{b[0]: 5.2f} y:{b[1]: 5.2f} z:{b[2]: 5.2f} a:{b[3]: 5.2f} b:{b[4]: 5.2f} c:{b[5]: 5.2f} grp:{b[6]: 5.2f}"
+    c = npz['robot_obs']
+    robot_obs   = f"tcp:  x:{c[0]: 5.2f} y:{c[1]: 5.2f} z:{c[2]: 5.2f} a:{c[3]: 5.2f} b:{c[4]: 5.2f} c:{c[5]: 5.2f} grp:{c[6]*100: 5.2f}"
+    robot_arm   = f"arm:  a:{c[7]: 5.2f} b:{c[8]: 5.2f} c:{c[9]: 5.2f} d:{c[10]: 5.2f} e:{c[11]: 5.2f} f:{c[12]: 5.2f} g:{c[13]: 5.2f} grp:{c[14]: 5.2f}"
+    d = npz['scene_obs']
+    red         = f"red:  x:{d[6]: 5.2f} y:{d[7]: 5.2f} z:{d[8]: 5.2f} a:{d[9]: 5.2f} b:{d[10]: 5.2f} c:{d[11]: 5.2f}"
+    blue        = f"blue: x:{d[12]: 5.2f} y:{d[13]: 5.2f} z:{d[14]: 5.2f} a:{d[15]: 5.2f} b:{d[16]: 5.2f} c:{d[17]: 5.2f}"
+    pink        = f"pink: x:{d[18]: 5.2f} y:{d[19]: 5.2f} z:{d[20]: 5.2f} a:{d[21]: 5.2f} b:{d[22]: 5.2f} c:{d[23]: 5.2f}"
+    desk        = f"door:{d[0]: 5.2f} drawer:{d[1]: 5.2f} button:{d[2]: 5.2f} switch:{d[3]: 5.2f} bulb:{d[4]: 5.2f} green:{d[5]: 5.2f}"
+    return "\n".join((indexstr, actions, rel_actions, robot_obs, robot_arm, red, blue, pink, desk))
+
+def update_frame(value):
+    index = int(value)
+    idnum = idnums[index]
+    npz = np.load(f"episode_{idnum}.npz", allow_pickle=True)
+    tklabels[1].image = array2image(npz['rgb_static'])
+    tklabels[1].config(image = tklabels[1].image)
+    tklabels[3].image = array2image(npz['depth_static'])
+    tklabels[3].config(image = tklabels[3].image)
+    tklabels[5].image = array2image(npz['rgb_gripper'])
+    tklabels[5].config(image = tklabels[5].image)
+    tklabels[7].image = array2image(npz['depth_gripper'])
+    tklabels[7].config(image = tklabels[7].image)
+    tklabels[9].image = array2image(npz['rgb_tactile'][:,:,0:3])
+    tklabels[9].config(image = tklabels[9].image)
+    tklabels[11].image = array2image(npz['rgb_tactile'][:,:,3:6])
+    tklabels[11].config(image = tklabels[11].image)
+    tklabels[13].image = array2image(npz['depth_tactile'][:,:,0])
+    tklabels[13].config(image = tklabels[13].image)
+    tklabels[15].image = array2image(npz['depth_tactile'][:,:,1])
+    tklabels[15].config(image = tklabels[15].image)
+    tklabels[16].text = numeric_fields(npz, idnum)
+    tklabels[16].config(text = tklabels[16].text)
+
+
+# Read filenames:
+idnums = []
+for f in tqdm(sorted(os.listdir('.'))):
+    m = re.match(r"episode_(\d{7})\.npz", f)
+    if m is not None:
+        idnum = m.group(1)
+        idnums.append(idnum)
+
+# Arrange the windows:
+row = [ tk.Frame(root) for i in range(4) ]
+frm = [ [ tk.Frame(row[0]) for i in range(3) ],
+        [ tk.Frame(row[1]) for i in range(4) ],
+        [ tk.Frame(row[2]) for i in range(1) ],
+        [ tk.Frame(row[3]) for i in range(1) ] ]
+for i in range(4):
+    row[i].pack()
+    for j in range(len(frm[i])):
+        frm[i][j].pack(side="left")
+
+tklabels = [
+    tk.Label(frm[0][0], text="rgb_static"),
+    tk.Label(frm[0][0]), #, image=rgb_static),
+    tk.Label(frm[0][1], text="depth_static"),
+    tk.Label(frm[0][1]), # , image=depth_static),
+    tk.Label(frm[0][2], text="rgb_gripper"),
+    tk.Label(frm[0][2]), # , image=rgb_gripper),
+    tk.Label(frm[0][2], text="depth_gripper"),
+    tk.Label(frm[0][2]), # , image=depth_gripper),
+    tk.Label(frm[1][0], text="rgb_tactile1"),
+    tk.Label(frm[1][0]), # , image=rgb_tactile1),
+    tk.Label(frm[1][1], text="rgb_tactile2"),
+    tk.Label(frm[1][1]), # , image=rgb_tactile2),
+    tk.Label(frm[1][2], text="depth_tactile1"),
+    tk.Label(frm[1][2]), # , image=depth_tactile1),
+    tk.Label(frm[1][3], text="depth_tactile2"),
+    tk.Label(frm[1][3]), # , image=depth_tactile2),
+    tk.Label(frm[2][0], text='', justify="left", anchor="nw")
+]
+
+for lbl in tklabels:
+    lbl.pack()
+
+# Slider:
+slider = tk.Scale(frm[3][0], from_=0, to=len(idnums)-1, orient=tk.HORIZONTAL, command=update_frame, length=500, resolution=1.0)
+slider.pack()
+
+# Initialize with first frame
+update_frame(0)
+
+# Start the main event loop
+tk.mainloop()
