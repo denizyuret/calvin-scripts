fixed tactile normalization

model/loaddata.py
@@ -12,15 +12,22 @@ def loaddata(prefix):
     with gzip.open(prefix + '-controllers.tsv.gz', 'rt') as f:
         cont = np.loadtxt(f, delimiter='\t', dtype='float32')
         assert np.array_equal(data[:,0], cont[:,0]), 'cont indices do not match'
-    with gzip.open(prefix + '-tactile.tsv.gz', 'rt') as f:
+    with gzip.open(prefix + '-tactile2.tsv.gz', 'rt') as f:
         tact = np.loadtxt(f, delimiter='\t', dtype='float32')
         assert np.array_equal(data[:,0], tact[:,0]), 'tact indices do not match'
     data = np.concatenate((data, cont[:,1:], tact[:,1:]), axis=1)
+    data = normalize(data)
     pos2id = data[:,0].astype(int)
     id2pos = np.full(1+max(pos2id), -1)
     for (pos, id) in enumerate(pos2id):
         id2pos[id] = pos
     return data, pos2id, id2pos
+
+
+def normalize(data):
+    data[:,32:34] = data[:,32:34] * 10.0  # button and switch
+    data[:,21] = data[:,21] * 10.0        # gripper opening
+    return data
 
 
 def loadlang(prefix):

model/mlp.py
@@ -1,63 +1,64 @@
 import torch
-from loaddata import calvindataset1
-from torch.utils.data import DataLoader
 import torch.nn as nn
 import pytorch_lightning as pl
+from loaddata import calvindataset1
+from torch.utils.data import DataLoader
 from pytorch_lightning.loggers import WandbLogger
-import torchmetrics
+from torchmetrics.functional import accuracy
+from torch.nn.functional import cross_entropy
 
 
 class LitMLP(pl.LightningModule):
     def __init__(self, sizes):
         super().__init__()
+        self.sizes = sizes
         self.mlp = nn.Sequential()
         for i in range(1, len(sizes)-1):
             self.mlp.append(nn.Linear(sizes[i-1], sizes[i]))
             self.mlp.append(nn.ReLU())
         self.mlp.append(nn.Linear(sizes[-2], sizes[-1]))
-        self.trn_acc = torchmetrics.Accuracy(task="multiclass", num_classes=sizes[-1])
-        self.val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=sizes[-1])
-        self.save_hyperparameters()
 
-    def training_step(self, batch, batch_idx):
+    def step(self, batch, batch_idx):
         x, y = batch
         yhat = self.mlp(x)
-        self.trn_acc.update(yhat, y)
-        #self.log("trn_acc", self.trn_acc) # , on_step=False, on_epoch=True)
-        loss = nn.functional.cross_entropy(yhat, y)
-        self.log("trn_loss", loss)
-        return loss
+        loss = cross_entropy(yhat, y)
+        acc = accuracy(yhat, y, task='multiclass', num_classes=self.sizes[-1])
+        return {'loss': loss, 'acc': acc}
+
+    def epoch_end(self, outputs, log_prefix):
+        loss = torch.stack([output["loss"] for output in outputs]).mean()
+        acc = torch.stack([output["acc"] for output in outputs]).mean()
+        self.log(log_prefix + "_loss", loss, on_step=False, on_epoch=True)
+        self.log(log_prefix + "_acc",   acc, on_step=False, on_epoch=True)
+    
+    def training_step(self, batch, batch_idx):
+        return self.step(batch, batch_idx)
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        yhat = self.mlp(x)
-        self.val_acc.update(yhat, y)
-        #self.log("val_acc", self.val_acc) # , on_step=False, on_epoch=True)
-        loss = nn.functional.cross_entropy(yhat, y)
-        self.log("val_loss", loss)
+        return self.step(batch, batch_idx)
 
     def training_epoch_end(self, outputs):
-        self.log("trn_acc", self.trn_acc.compute())
-        self.trn_acc.reset()
+        self.epoch_end(outputs, 'trn')
 
     def validation_epoch_end(self, outputs):
-        self.log("val_acc", self.val_acc.compute())
-        self.val_acc.reset()
+        self.epoch_end(outputs, 'val')
         
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
 
-def train(pre = '../data/D', batch_size=128, hidden=[128]):
+def train(pre = '../data/D', batch_size=128, hidden=[128], features=range(1,74), window=32, name='001', max_epochs=32):
     global mlp
     torch.set_float32_matmul_precision('medium')
-    trn = DataLoader(calvindataset1(prefix = pre + '-training'), batch_size=batch_size, shuffle=True, num_workers=6)
-    val = DataLoader(calvindataset1(prefix = pre + '-validation'), batch_size=batch_size, shuffle=False, num_workers=6)
+    trn = calvindataset1(prefix = pre + '-training',   features=features, window=window)
+    val = calvindataset1(prefix = pre + '-validation', features=features, window=window)
+    trn = DataLoader(trn, batch_size=batch_size, shuffle=True, num_workers=6)
+    val = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=6)
     xsize = trn.dataset.tensors[0].shape[1]
     ysize = 1 + max(max(trn.dataset.tensors[1]).item(), max(val.dataset.tensors[1]).item())
     mlp = LitMLP((xsize, *hidden, ysize))
-    wandb_logger = WandbLogger(project='mlp_project')
+    wandb_logger = WandbLogger(project='mlp_project', name=name)
     wandb_logger.experiment.config["batch_size"] = batch_size
-    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=32, logger=wandb_logger)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=max_epochs, logger=wandb_logger)
     trainer.fit(mlp, trn, val)