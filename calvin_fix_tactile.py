b92f3062c3f613c3bb08a437fcaa5c1a0612c482
Deniz Yuret <denizyuret@gmail.com>
2023-03-03 09:29:49 +0300

    *** empty log message ***

--- calvin_fix_tactile.py
+++ calvin_fix_tactile.py
@@ -2,9 +2,14 @@
 
 import sys
 import numpy as np
+from io import StringIO
 
-a = np.loadtxt(sys.stdin, delimiter='\t')
-a[:,1:3] = a[:,1:3] * 100.0
-a[:,3:9] = a[:,3:9] / 255.0
-np.savetxt(sys.stdout, a, delimiter='\t', fmt='%g')
-
+for line in sys.stdin:
+    fields = line.strip().split('\t')
+    nfields = np.loadtxt(StringIO(line), delimiter='\t')
+    nfields[1:3] = nfields[1:3] * 100.0
+    nfields[3:9] = nfields[3:9] / 255.0
+    print(fields[0], end='\t')
+    for f in nfields[1:]:
+        print(f"\t{f:g}", end='')
+    print('')
