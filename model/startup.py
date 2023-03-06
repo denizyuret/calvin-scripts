beca744ac9171f175b37c762365f781f07e7f5b3
Deniz Yuret <denizyuret@gmail.com>
2023-03-07 01:05:49 +0300

    .

--- model/startup.py
+++ model/startup.py
@@ -9,7 +9,8 @@ import pytorch_lightning as pl
 from torchmetrics.functional import accuracy
 from torch.nn.functional import cross_entropy
 from torch.utils.data import DataLoader
-dtrn = ld.calvindataset2("../data/D-training")
-dval = ld.calvindataset2("../data/D-validation")
-abcval = ld.calvindataset2("../data/ABC-validation")
+tr = pl.Trainer(accelerator='gpu', devices=1, max_epochs=1, enable_progress_bar=False)
+# dtrn = ld.calvindataset("../data/D-training")
+# dval = ld.calvindataset("../data/D-validation")
+# abcval = ld.calvindataset("../data/ABC-validation")
 # abcdval = ld.calvindataset2("../data/ABCD-validation") # same as abcval
