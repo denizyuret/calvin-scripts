af977a944f33e435072b2186932d7bf52da4d5f6
Deniz Yuret <denizyuret@gmail.com>
2023-03-08 16:56:34 +0300

    .

--- model/rnn.py
+++ model/rnn.py
@@ -1,11 +1,8 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-import wandb
-import loaddata as ld
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
-from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torchmetrics import MeanMetric, MaxMetric, Accuracy
 from torch.nn.functional import cross_entropy, softmax
@@ -26,10 +23,10 @@ class LitRNN(pl.LightningModule):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bias, batch_first, dropout)
         self.proj = nn.Sequential(nn.Dropout(p=dropout), nn.Linear(hidden_size, num_classes))
 
-        self.trn_loss = MeanMetric()
-        self.val_loss = MeanMetric()
-        self.trn_acc = Accuracy(task = 'multiclass', num_classes = self.num_classes)
-        self.val_acc = Accuracy(task = 'multiclass', num_classes = self.num_classes)
+        self.trn_loss = [ MeanMetric() for _ in range(3) ]
+        self.val_loss = [ MeanMetric() for _ in range(3) ]
+        self.trn_acc = [ Accuracy(task = 'multiclass', num_classes = self.num_classes) for _ in range(3) ]
+        self.val_acc = [ Accuracy(task = 'multiclass', num_classes = self.num_classes) for _ in range(3) ]
         self.wnorm = MaxMetric()
 
     def configure_optimizers(self):
@@ -38,41 +35,66 @@ class LitRNN(pl.LightningModule):
 
     def forward(self, x):
         lstm_out, (h_n, c_n) = self.lstm(x)
-        if self.output_interval > 0:
-            lstm_out = lstm_out[:,-self.output_interval:,:] # (B,T,C) => (B,O,C)
         return self.proj(lstm_out)
 
-    def step(self, batch, loss, acc, wnorm):
-        x, y, *rest = batch     # x=(B,T,X), y=(B)
-        yhat = self(x)          # yhat=(B,O,C)
-        y = y.repeat_interleave(yhat.shape[1])      # y->(B*O)
-        yhat = yhat.view(-1, self.num_classes)      # yhat->(B*O,C)
-        xent = cross_entropy(yhat, y)
-        loss.update(xent, len(y))
-        acc.update(yhat, y)
-        wnorm.update(torch.linalg.vector_norm(self.proj[-1].weight))
-        return xent
-
-    def training_step(self, batch, batch_idx):
-        return self.step(batch, self.trn_loss, self.trn_acc, self.wnorm)
-
-    def validation_step(self, batch, batch_idx):
-        return self.step(batch, self.val_loss, self.val_acc, self.wnorm)
-
-    def training_epoch_end(self, outputs):
-        self.log("trn_loss", self.trn_loss.compute())
-        self.log("trn_acc",  self.trn_acc.compute())
-        self.log("wnorm",    self.wnorm.compute())
-        self.trn_loss.reset(); self.trn_acc.reset(); self.wnorm.reset()
-
-    def validation_epoch_end(self, outputs):
-        self.log("val_loss", self.val_loss.compute())
-        self.log("val_acc",  self.val_acc.compute())
-        self.val_loss.reset(); self.val_acc.reset(); 
-    
     def predict_step(self, batch, batch_idx):
         x, *rest = batch
         return (softmax(self(x), dim=2), *rest)
+
+    def step(self, batch, loss, acc):
+        x, y, *rest = batch     # x=(B,T,X), y=(B)
+        yhat = self(x)          # yhat=(B,T,C)
+        # All frames
+        yhat0 = yhat.view(-1, yhat.shape[-1])
+        y0 = y.repeat_interleave(yhat.shape[1])
+        loss0 = cross_entropy(yhat0, y0)
+        loss[0].update(loss0.cpu(), len(y0))
+        acc[0].update(yhat0.cpu(), y0.cpu())
+        # Last frame
+        yhat1 = yhat[:,-1,:]
+        y1 = y
+        loss1 = cross_entropy(yhat1, y1)
+        loss[1].update(loss1.cpu(), len(y1))
+        acc[1].update(yhat1.cpu(), y1.cpu())
+        # Last output_interval frames
+        if self.output_interval <= 0 or self.output_interval >= yhat.shape[1]:
+            yhat2, y2, loss2 = yhat0, y0, loss0
+        elif self.output_interval == 1:
+            yhat2, y2, loss2 = yhat1, y1, loss1
+        else:
+            yhat2 = yhat[:,-self.output_interval:,:].reshape(-1, yhat.shape[-1])
+            y2 = y.repeat_interleave(self.output_interval)
+            loss2 = cross_entropy(yhat2, y2)
+        loss[2].update(loss2.cpu(), len(y2))
+        acc[2].update(yhat2.cpu(), y2.cpu())
+        return loss2
+
+    def epoch_end(self, pre, loss, acc):
+        self.log(pre+"_loss_all", loss[0].compute())
+        self.log(pre+"_acc_all",  acc[0].compute())
+        self.log(pre+"_loss_1", loss[1].compute())
+        self.log(pre+"_acc_1",  acc[1].compute())
+        self.log(pre+"_loss_n", loss[2].compute())
+        self.log(pre+"_acc_n",  acc[2].compute())
+        for x in (*loss, *acc):
+            x.reset()
+
+    def training_step(self, batch, batch_idx):
+        self.wnorm.update(torch.linalg.vector_norm(self.proj[-1].weight))
+        return self.step(batch, self.trn_loss, self.trn_acc)
+
+    def validation_step(self, batch, batch_idx):
+        self.step(batch, self.val_loss, self.val_acc)
+
+    def training_epoch_end(self, outputs):
+        self.epoch_end("trn", self.trn_loss, self.trn_acc)
+        self.log("wnorm", self.wnorm.compute())
+        self.wnorm.reset()
+
+    def validation_epoch_end(self, outputs):
+        self.epoch_end("val", self.val_loss, self.val_acc)
+    
+
 
 
 # Given trn and val datasets and optional hyperparameters, train and return an rnn
@@ -84,12 +106,7 @@ def train(trn_set, val_set, batch_size=128, max_epochs=-1, max_steps=-1, name=No
     rnn = LitRNN(input_size, hidden_size, num_classes, num_layers, bias, batch_first, dropout, weight_decay, lr, output_interval)
     trn_loader = DataLoader(trn_set, batch_size, shuffle=True,  num_workers=6)
     val_loader = DataLoader(val_set, batch_size, shuffle=False, num_workers=6)
-    wandb_logger = WandbLogger(project='mlp_project', name=name)
-    wandb_logger.experiment.config["batch_size"] = batch_size
+    checkpoint_callback = ModelCheckpoint(monitor = "val_acc_1", mode = 'max')
     torch.set_float32_matmul_precision('medium')
-    checkpoint_callback = ModelCheckpoint(monitor = "val_acc", mode = 'max')
-    trainer = pl.Trainer(accelerator='gpu', devices=1, callbacks=[checkpoint_callback], max_epochs=max_epochs, max_steps=max_steps, logger=wandb_logger)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=max_epochs, max_steps=max_steps, callbacks=[checkpoint_callback])
     trainer.fit(rnn, trn_loader, val_loader)
-    wandb.finish()
-
-
