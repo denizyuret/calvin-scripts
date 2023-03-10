5a991c67f6e119b8287f861dd5853339530ffe5d
Deniz Yuret <denizyuret@gmail.com>
2023-03-10 22:03:28 +0300

    .

---
+++ model/eval.py
@@ -0,0 +1,26 @@
+import sys
+from argparse import ArgumentParser
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+import loaddata as ld
+import mlp
+import rnn
+
+parser = ArgumentParser(description="Evaluate a model on the D/validation dataset")
+parser.add_argument("model", type=str, help="Model checkpoint")
+parser.add_argument("-d", "--data", type=str, default="../data/D-validation")
+parser.add_argument("-i", "--instances_per_episode",  type=int, default=1)
+parser.add_argument("-c", "--context_length",  type=int, default=64)
+parser.add_argument("-f", "--features",  type=str, default="range(1,74)")
+args = parser.parse_args()
+
+try:
+    model = mlp.LitMLP.load_from_checkpoint(args.model)
+except TypeError:
+    model = rnn.LitRNN.load_from_checkpoint(args.model)
+except:
+    error('Cannot load model')
+
+data = ld.CalvinDataset(args.data, eval(args.features), instances_per_episode, context_length)
+tr = pl.Trainer(accelerator='gpu', devices=1, max_epochs=1)
+tr.validate(model, DataLoader(data, batch_size=128))
--- model/top5.py
+++ model/top5.py
@@ -1,15 +1,25 @@
 import sys
 import torch
+import loaddata as ld
+import torch
 from torch.nn.functional import softmax
 from loaddata import int2task
+import rnn
+import mlp
 
-def top5(model, dataset):
-    (x,y,idx) = dataset.tensors
-    yhat = model(x)
-    prob = softmax(yhat, dim=1)
+def top5(model, prefix='../data/D-validation', features=range(1,74)):
+    model.eval()
+    (data,pos2id,id2pos) = ld.loaddata(prefix)
+    x = torch.tensor(data[:,features])
+    if isinstance(model, rnn.LitRNN):
+        x = x.reshape((1, *x.shape))
+    y = model(x)
+    if isinstance(model, rnn.LitRNN):
+        y = y.reshape(*y.shape[1:])
+    prob = softmax(y, dim=1)
     with open('top5.out', 'w') as f:
-        for i in range(len(idx)):
-            print(idx[i].item(), end='', file=f)
+        for i in range(len(pos2id)):
+            print(pos2id[i], end='', file=f)
             iprob = prob[i,:]
             (_, taskids) = torch.sort(iprob, descending=True)
             for j in range(5):
