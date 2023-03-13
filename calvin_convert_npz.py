fixed normalization: npz has unnormalized data, CalvinDataset.__getitem__ does the normalization

calvin_convert_npz.py
@@ -3,9 +3,26 @@
 # Convert tsv data to npz format
 
 import sys
+import gzip
 import numpy as np
 import loaddata as ld
 from argparse import ArgumentParser
+
+
+def loaddata_simple(prefix):    # load without normalization
+    with gzip.open(prefix + '.tsv.gz', 'rt') as f:
+        data = np.loadtxt(f, delimiter='\t', dtype='float32')
+    with gzip.open(prefix + '-controllers.tsv.gz', 'rt') as f:
+        cont = np.loadtxt(f, delimiter='\t', dtype='float32')
+        assert np.array_equal(data[:,0], cont[:,0]), 'cont indices do not match'
+    with gzip.open(prefix + '-tactile2.tsv.gz', 'rt') as f:
+        tact = np.loadtxt(f, delimiter='\t', dtype='float32')
+        assert np.array_equal(data[:,0], tact[:,0]), 'tact indices do not match'
+    with gzip.open(prefix + '-sdiff.tsv.gz', 'rt') as f:
+        sdiff = np.loadtxt(f, delimiter='\t', dtype='float32')
+        assert np.array_equal(data[:,0], sdiff[:,0]), 'sdiff indices do not match'
+    return np.concatenate((data, cont[:,1:], tact[:,1:], sdiff[:,1:]), axis=1)
+
 
 parser = ArgumentParser(description="Convert CALVIN dataset to npz format")
 parser.add_argument("dataset", type=str, default="debug", help="debug|D|ABC|ABCD")
@@ -16,33 +33,34 @@ args = parser.parse_args()
 
 ep_start_end_path = f"{args.calvin}/{args.dataset}/{args.split}/ep_start_end_ids.npy"
 print(f"Reading {ep_start_end_path}...", file=sys.stderr)
-ep_start_end_ids = np.load(ep_start_end_path)
+episodes = np.load(ep_start_end_path).astype('int32')
 
+tasknames = np.array(ld.int2task, dtype='O')
+task2int = {}
+for (i,task) in enumerate(ld.int2task):
+    task2int[task] = i
+
+lang_path = f"{args.calvin}/{args.dataset}/{args.split}/lang_annotations/auto_lang_ann.npy"
+print(f"Reading {lang_path}...", file=sys.stderr)
+a = np.load(lang_path, allow_pickle=True).item()
+lang = np.array([(start, end, task2int[task], annot) for
+                 ((start, end), task, annot) in zip(a['info']['indx'], a['language']['task'], a['language']['ann'])],
+                dtype=[('start', '<i4'), ('end', '<i4'), ('taskid', '<i8'), ('instruction', 'O')])
+         
 tsv_gz_path = f"{args.data}/{args.dataset}-{args.split}"
 print(f"Reading {tsv_gz_path}...", file=sys.stderr)
-data, pos2id, id2pos = ld.loaddata(tsv_gz_path)
-lang, task2int, int2task = ld.loadlang(tsv_gz_path)
-assert np.array_equal(pos2id, data[:,0].astype(int))
-
-print(f"Cooking data...", file=sys.stderr)
-# replace task names with task ids in lang:
-lang = np.array([(start, end, task2int[task], annot) for (start, end, task, annot) in lang],
-                dtype=[('start', '<i4'), ('end', '<i4'), ('taskid', '<i8'), ('instruction', 'O')])
+data = loaddata_simple(tsv_gz_path)
+fieldnames = np.array([x[0] for x in ld.dtype_data[1:] + ld.dtype_cont[1:] + ld.dtype_tact[1:] + ld.dtype_sdiff[1:]], dtype='O')
 
 # cut the buggy extra frames from ABCD-training:
 if args.dataset == "ABCD" and args.split == "training":
-    searchlist = pos2id.tolist()
+    searchlist = data[:,0].astype('int32').tolist()
     delete_rows = [ searchlist.index(x) for x in (37682, 53818, 244284, 420498) ]
     data = np.delete(data, delete_rows, axis=0)
 
 # first column of data is frame_id and should be cut
 frameids = data[:,0].astype('int32')
 data = data[:,1:]
-
-# collect column names and task names
-fieldnames = np.array([x[0] for x in ld.dtype_data[1:] + ld.dtype_cont[1:] + ld.dtype_tact[1:] + ld.dtype_sdiff[1:]], dtype='O')
-tasknames = np.array(int2task, dtype='O')
-episodes = ep_start_end_ids.astype('int32')
 
 output_path = f"{args.data}/{args.dataset}-{args.split}.npz"
 print(f"Saving to {output_path}...", file=sys.stderr)

calvindataset.py
@@ -20,7 +20,8 @@ class CalvinDataset(Dataset):
         tasknames = npzfile['tasknames'].tolist()
         fieldnames = npzfile['fieldnames'].tolist()
         episodes = npzfile['episodes']
-
+        normalized_fields = [ fieldnames.index(f) for f in ('tcpg', 'button', 'switch') ]
+                
         # build reverse maps
         id2pos = np.full(1+max(frameids), -1)
         for (pos, id) in enumerate(frameids):
@@ -43,13 +44,19 @@ class CalvinDataset(Dataset):
 
     def __getitem__(self, index):
         i = self.data_index[index]
-        x = self.data[np.ix_(range(1+i-self.context_length, 1+i), self.features)]
-        inputs = torch.from_numpy(x)
+        inputs = self.data[np.ix_(range(1+i-self.context_length, 1+i), self.features)]
+        inputs = self.normalize(inputs)
+        inputs = torch.from_numpy(inputs)
         target = torch.tensor(self.target[index])
         frame = torch.tensor(self.frame[index])
         return (inputs, target, frame)
 
     def __len__(self):
         return len(self.target)
+
+    def normalize(self, inputs):
+        inputs[:,self.normalized_fields] = inputs[:,self.normalized_fields] * 10.0
+        return inputs
+
 
 # (0, 'actx'),(1, 'acty'),(2, 'actz'),(3, 'acta'),(4, 'actb'),(5, 'actc'),(6, 'actg'),(7, 'relx'),(8, 'rely'),(9, 'relz'),(10, 'rela'),(11, 'relb'),(12, 'relc'),(13, 'relg'),(14, 'tcpx'),(15, 'tcpy'),(16, 'tcpz'),(17, 'tcpa'),(18, 'tcpb'),(19, 'tcpc'),(20, 'tcpg'),(21, 'arm1'),(22, 'arm2'),(23, 'arm3'),(24, 'arm4'),(25, 'arm5'),(26, 'arm6'),(27, 'arm7'),(28, 'armg'),(29, 'slider'),(30, 'drawer'),(31, 'button'),(32, 'switch'),(33, 'lightbulb'),(34, 'greenlight'),(35, 'redx'),(36, 'redy'),(37, 'redz'),(38, 'reda'),(39, 'redb'),(40, 'redc'),(41, 'bluex'),(42, 'bluey'),(43, 'bluez'),(44, 'bluea'),(45, 'blueb'),(46, 'bluec'),(47, 'pinkx'),(48, 'pinky'),(49, 'pinkz'),(50, 'pinka'),(51, 'pinkb'),(52, 'pinkc'),(53, 'slider.x'),(54, 'slider.y'),(55, 'slider.z'),(56, 'drawer.x'),(57, 'drawer.y'),(58, 'drawer.z'),(59, 'button.x'),(60, 'button.y'),(61, 'button.z'),(62, 'switch.x'),(63, 'switch.y'),(64, 'switch.z'),(65, 'tact1d'),(66, 'tact2d'),(67, 'tact1r'),(68, 'tact1g'),(69, 'tact1b'),(70, 'tact2r'),(71, 'tact2g'),(72, 'tact2b'),(73, 'slider_diff'),(74, 'drawer_diff'),(75, 'button_diff'),(76, 'switch_diff'),(77, 'lightbulb_diff'),(78, 'greenlight_diff'),(79, 'redx_diff'),(80, 'redy_diff'),(81, 'redz_diff'),(82, 'reda_diff'),(83, 'redb_diff'),(84, 'redc_diff'),(85, 'bluex_diff'),(86, 'bluey_diff'),(87, 'bluez_diff'),(88, 'bluea_diff'),(89, 'blueb_diff'),(90, 'bluec_diff'),(91, 'pinkx_diff'),(92, 'pinky_diff'),(93, 'pinkz_diff'),(94, 'pinka_diff'),(95, 'pinkb_diff'),(96, 'pinkc_diff')

eval.py
@@ -44,7 +44,6 @@ index = torch.cat([x[2] for x in p])
 num_classes = 34
 if isinstance(model, rnn.LitRNN):
     preds = preds[:,-1,:]
-print(multiclass_accuracy(preds, target, num_classes, average='micro').item())
 a = multiclass_accuracy(preds, target, num_classes, average='none')
 c = multiclass_confusion_matrix(preds, target, num_classes)
 for j in range(num_classes):
@@ -57,6 +56,8 @@ for i in range(num_classes):
         else:
             print(f"{c[i,j]:>3}", end="")
     print(f"{i:>3} {data.tasknames[i]} ({a[i]:.4f})")
+
+print(multiclass_accuracy(preds, target, num_classes, average='micro').item())
 
 print(f"Saving data and predictions in eval.out...", file=sys.stderr)
 (x, *rest) = next(iter(DataLoader(data, batch_size=len(data))))