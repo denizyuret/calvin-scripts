1856c7c30b8df86ff344bb802ce177dc2c1c920f
Deniz Yuret <denizyuret@gmail.com>
2023-03-16 09:58:21 +0300

    .

--- calvindataset.py
+++ calvindataset.py
@@ -2,7 +2,13 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-# Defines CalvinDataset, TwoFrameDataset.
+
+# The dataset can be carved up in the following ways (only some of which are implemented):
+# 1. Features: we can take a subset of features, or automatically create new (difference, sum) features.
+# 2. Context Frames: we can include all of, or a subset of n frames prior to the target frame as context.
+# 3. Target Frames: we can consider last 1 or last n frames of an episode as instances for prediction.
+# 4. Instances: we can include all or a subsample of instances per class. (for learning curve).
+
 
 # Features: (0, 'actx'),(1, 'acty'),(2, 'actz'),(3, 'acta'),(4, 'actb'),(5, 'actc'),(6, 'actg'),(7, 'relx'),(8, 'rely'),(9, 'relz'),(10, 'rela'),(11, 'relb'),(12, 'relc'),(13, 'relg'),(14, 'tcpx'),(15, 'tcpy'),(16, 'tcpz'),(17, 'tcpa'),(18, 'tcpb'),(19, 'tcpc'),(20, 'tcpg'),(21, 'arm1'),(22, 'arm2'),(23, 'arm3'),(24, 'arm4'),(25, 'arm5'),(26, 'arm6'),(27, 'arm7'),(28, 'armg'),(29, 'slider'),(30, 'drawer'),(31, 'button'),(32, 'switch'),(33, 'lightbulb'),(34, 'greenlight'),(35, 'redx'),(36, 'redy'),(37, 'redz'),(38, 'reda'),(39, 'redb'),(40, 'redc'),(41, 'bluex'),(42, 'bluey'),(43, 'bluez'),(44, 'bluea'),(45, 'blueb'),(46, 'bluec'),(47, 'pinkx'),(48, 'pinky'),(49, 'pinkz'),(50, 'pinka'),(51, 'pinkb'),(52, 'pinkc'),(53, 'slider.x'),(54, 'slider.y'),(55, 'slider.z'),(56, 'drawer.x'),(57, 'drawer.y'),(58, 'drawer.z'),(59, 'button.x'),(60, 'button.y'),(61, 'button.z'),(62, 'switch.x'),(63, 'switch.y'),(64, 'switch.z'),(65, 'tact1d'),(66, 'tact2d'),(67, 'tact1r'),(68, 'tact1g'),(69, 'tact1b'),(70, 'tact2r'),(71, 'tact2g'),(72, 'tact2b'),(73, 'slider_diff'),(74, 'drawer_diff'),(75, 'button_diff'),(76, 'switch_diff'),(77, 'lightbulb_diff'),(78, 'greenlight_diff'),(79, 'redx_diff'),(80, 'redy_diff'),(81, 'redz_diff'),(82, 'reda_diff'),(83, 'redb_diff'),(84, 'redc_diff'),(85, 'bluex_diff'),(86, 'bluey_diff'),(87, 'bluez_diff'),(88, 'bluea_diff'),(89, 'blueb_diff'),(90, 'bluec_diff'),(91, 'pinkx_diff'),(92, 'pinky_diff'),(93, 'pinkz_diff'),(94, 'pinka_diff'),(95, 'pinkb_diff'),(96, 'pinkc_diff')
 
